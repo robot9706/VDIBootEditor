bd44de1268d0292a022795eaeb0f3b68b3cf8847
Nagygyörgy Bence <robot9706@gmail.com>
2014-11-08 14:35:22 +0100

    Initial commit

---
+++ VDIBootEditor/VDIBootEditor/EditorForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace VDIBootEditor
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.vdiPath = new System.Windows.Forms.TextBox();
+            this.openVDI = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "VDI file:";
+            // 
+            // vdiPath
+            // 
+            this.vdiPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vdiPath.BackColor = System.Drawing.Color.White;
+            this.vdiPath.Location = new System.Drawing.Point(66, 13);
+            this.vdiPath.Name = "vdiPath";
+            this.vdiPath.ReadOnly = true;
+            this.vdiPath.Size = new System.Drawing.Size(344, 21);
+            this.vdiPath.TabIndex = 3;
+            // 
+            // openVDI
+            // 
+            this.openVDI.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openVDI.Location = new System.Drawing.Point(413, 12);
+            this.openVDI.Name = "openVDI";
+            this.openVDI.Size = new System.Drawing.Size(75, 23);
+            this.openVDI.TabIndex = 5;
+            this.openVDI.Text = "Open";
+            this.openVDI.UseVisualStyleBackColor = true;
+            this.openVDI.Click += new System.EventHandler(this.openVDI_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.Location = new System.Drawing.Point(243, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Write MBR to VDI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Location = new System.Drawing.Point(66, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Read MBR from VDI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EditorForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(497, 83);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.openVDI);
+            this.Controls.Add(this.vdiPath);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "EditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VDI MBR editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox vdiPath;
+        private System.Windows.Forms.Button openVDI;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VDIBootEditor/VDIBootEditor/EditorForm.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VDIBootEditor
+{
+    public partial class EditorForm : Form
+    {
+        private byte[] _vdiHeader = new byte[]
+        {
+            0x3C, 0x3C, 0x3C, 0x20, 0x4F, 0x72, 0x61, 0x63, 0x6C, 0x65, 0x20, 0x56, 0x4D, 0x20, 0x56, 0x69,
+            0x72, 0x74, 0x75, 0x61, 0x6C, 0x42, 0x6F, 0x78, 0x20, 0x44, 0x69, 0x73, 0x6B, 0x20, 0x49, 0x6D, 
+            0x61, 0x67, 0x65, 0x20, 0x3E, 0x3E, 0x3E
+        };
+
+        private int _mbrOffset;
+
+        public EditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openVDI_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "VirtualBox VDI files(*.vid)|*.vdi";
+            if (opf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (ValidImage(opf.FileName))
+                {
+                    vdiPath.Text = opf.FileName;
+                }
+                else
+                {
+                    vdiPath.Text = string.Empty;
+                    MessageBox.Show("Invalid VDI!");
+                }
+            }
+        }
+
+        private bool ValidImage(string path)
+        {
+            try
+            {
+                bool ok = false;
+                using (FileStream fs = File.OpenRead(path))
+                {
+                    if (fs.Length > _vdiHeader.Length)
+                    {
+                        byte[] header = new byte[_vdiHeader.Length];
+                        fs.Read(header, 0, header.Length);
+
+                        if (CompareArrays(_vdiHeader, header))
+                        {
+                            byte[] mbrPosition = new byte[4];
+                            fs.Position = 0x159;
+                            fs.Read(mbrPosition, 0, mbrPosition.Length);
+
+                            _mbrOffset = LittleEndianToInt(mbrPosition);
+                            ok = true;
+                        }
+                    }
+                }
+
+                return ok;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while checking VDI file: " + ex.Message);
+            }
+            return false;
+        }
+
+        private bool CompareArrays(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length)
+                return false;
+
+            for (int x = 0; x < a.Length; x++)
+            {
+                if (a[x] != b[x])
+                    return false;
+            }
+            return true;
+        }
+
+        private int LittleEndianToInt(byte[] data)
+        {
+            return (data[0] << 24)
+                 | (data[1] << 16)
+                 | (data[2] << 8)
+                 | data[3];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(vdiPath.Text))
+            {
+                MessageBox.Show("File not found: " + vdiPath.Text);
+                return;
+            }
+
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = "Bin (*.bin)|*.bin|Raw (*.raw)|*.raw|Img (*.img)|*.img";
+            if (svf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    using (FileStream raw = File.OpenWrite(svf.FileName))
+                    {
+                        using (FileStream vdi = File.OpenRead(vdiPath.Text))
+                        {
+                            vdi.Position = _mbrOffset;
+                            byte[] mbr = new byte[512];
+                            vdi.Read(mbr, 0, mbr.Length);
+
+                            raw.Write(mbr, 0, mbr.Length);
+                        }
+                    }
+                    MessageBox.Show("Done!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unable to read MBR!\n" + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(vdiPath.Text))
+            {
+                MessageBox.Show("File not found: " + vdiPath.Text);
+                return;
+            }
+
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "All binary files (*.bin,*.img,*.raw)|*.bin;*.img;*.raw|All files (*.*)|*.*";
+            if (opf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    bool ok = true;
+
+                    using (FileStream raw = File.OpenRead(opf.FileName))
+                    {
+                        if (raw.Length == 512)
+                        {
+                            using (FileStream vdi = File.OpenWrite(vdiPath.Text))
+                            {
+                                byte[] mbr = new byte[512];
+                                raw.Read(mbr, 0, mbr.Length);
+
+                                vdi.Position = _mbrOffset;
+                                vdi.Write(mbr, 0, mbr.Length);
+                            }
+                        }
+                        else
+                        {
+                            ok = false;
+                            MessageBox.Show("Only 512 byte MBRs are supported!");
+                        }
+                    }
+                    if (ok)
+                    {
+                        MessageBox.Show("Done!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unable to read MBR!\n" + ex.Message);
+                }
+            }
+        }
+    }
+}
